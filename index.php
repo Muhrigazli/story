4a56a45e14cde935ad40995adcc1245fe26f75e5
Muhriddin <nabiyevm74@gmail.com>
2025-04-29 10:34:33 +0500

    salom madihon

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php 
-
-class Car {
-
-}
-
-
-$bmw = new Car();
-
-
+require_once 'bootstrap.php';
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP Blog</title>
+</head>
+<body>
+    <h1>OOP Blog</h1>
+</body>
+</html>
---
+++ src/Post.php
@@ -0,0 +1,14 @@
+<?php
+class Post
+{
+    public $id;
+    public $title;
+    public $body;
+    public $created_at;
+}
+
+
+
+
+
+?>
