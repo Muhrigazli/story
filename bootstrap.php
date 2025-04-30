9614ffdfa1e09dbff7d4d9c8fa68e32b1f7f987f
Muhriddin <nabiyevm74@gmail.com>
2025-04-30 16:46:40 +0500

    300425 1646

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@ require_once 'autoload.php';
 
 $conn = new Database('localhost', 'oop-teach', 'root', '');
 
-
+Post::$pdo = $conn;
 echo 'success';
 
 
--- src/Post.php
+++ src/Post.php
@@ -1,10 +1,15 @@
 <?php
 class Post
 {
+    public static $pdo;
     public $id;
     public $title;
     public $body;
     public $created_at;
+
+    public function getAll(){
+         
+    }
 }
 
 
